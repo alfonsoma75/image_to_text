d208cec9208c9ffb71020071fd5117dbde21262b
alfonsoma75 <alfonsoma75@gmail.com>
2020-04-23 15:09:41 +0200

    [CHG] Cambia el nombre del archivo antes de ser guardado. El nombre será el campo unique_id.extensión.

--- itt/models.py
+++ itt/models.py
@@ -1,8 +1,22 @@
+import os
+
 from django.db import models
 
 
+def save_image_name(instance, filename):
+    """
+    Cambia el nombre del fichero para ser guardado con el formato UUID.extension
+    :param instance: obj, instancia del objeto que llama a la función.
+    :param filename: str, cadena con el nombre del fichero a modificar
+    :return: str, cadena con el formato final a guardar 'carpeta/UUID.extension'
+    """
+    print(filename)
+    name, extension = os.path.splitext(filename)
+    return 'tmp/{}{}'.format(instance.unique_id, extension)
+
+
 class ImageModel(models.Model):
-    image = models.ImageField(upload_to='tmp/')
+    image = models.ImageField(upload_to=save_image_name)
     unique_id = models.CharField(max_length=100, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
